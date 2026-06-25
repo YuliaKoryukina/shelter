3fbd0d5f9fe672414a0dcb823961c11b80e8092d
Юлия Корюкина <koryuckina.yu@yandex.ru>
2026-06-25 13:39:20 +0200

    fix: animation open menu

--- shelter/index.js
+++ shelter/index.js
@@ -5,10 +5,15 @@ hamburger.addEventListener('click', () => {
   nav.classList.toggle('navigation--open');
   hamburger.classList.toggle('hamburger--open'); 
 });
+
+function closeMenu() {
+  nav.classList.remove('navigation--open');
+  hamburger.classList.remove('hamburger--open');
+}
+
 document.querySelectorAll('.navigation_link').forEach(link => {
   link.addEventListener('click', () => {
-    nav.classList.remove('navigation--open');
-    hamburger.classList.remove('hamburger--open');
+    closeMenu();
   });
 });
 
@@ -16,7 +21,6 @@ document.querySelectorAll('.navigation_link').forEach(link => {
 document.addEventListener('click', (event) => {
   // Если кликнули МИМО навигации И МИМО самого бургера, закрываем меню
   if (!nav.contains(event.target) && !hamburger.contains(event.target)) {
-    nav.classList.remove('navigation--open');
-    hamburger.classList.remove('hamburger--open');
+    closeMenu();
   }
 });
