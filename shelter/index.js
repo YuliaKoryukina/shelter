bfc7768e4564d665c2909eaf109ae51db8d1a068
Юлия Корюкина <koryuckina.yu@yandex.ru>
2026-06-25 13:30:47 +0200

    fix: index.js

--- shelter/index.js
+++ shelter/index.js
@@ -3,5 +3,20 @@ const nav = document.querySelector('.navigation');
 
 hamburger.addEventListener('click', () => {
   nav.classList.toggle('navigation--open');
-  hamburger.classList.toggle('hamburger--open'); // Переключаем класс для поворота
+  hamburger.classList.toggle('hamburger--open'); 
+});
+document.querySelectorAll('.navigation_link').forEach(link => {
+  link.addEventListener('click', () => {
+    nav.classList.remove('navigation--open');
+    hamburger.classList.remove('hamburger--open');
+  });
+});
+
+// 2. Закрытие при клике на оверлей (вне меню и вне бургера)
+document.addEventListener('click', (event) => {
+  // Если кликнули МИМО навигации И МИМО самого бургера, закрываем меню
+  if (!nav.contains(event.target) && !hamburger.contains(event.target)) {
+    nav.classList.remove('navigation--open');
+    hamburger.classList.remove('hamburger--open');
+  }
 });
