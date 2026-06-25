0af02b5385922aa5be175f599863bef8255c08d6
Юлия Корюкина <koryuckina.yu@yandex.ru>
2026-06-25 11:24:26 +0200

    feat: Opening a menu using index.js

--- shelter/index.js
+++ shelter/index.js
@@ -1,3 +1,6 @@
-console.log(
-  "Markup validation - +10 \n Layout matches the design - +30 \n  CSS requirements - +15 \n Interactivity - +10 \n Markup validation - +10 \n Layout matches the design - +15 \n CSS requirements - +5 \n Interactivity - +10"
-);
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.navigation');
+
+hamburger.addEventListener('click', () => {
+  nav.classList.toggle('navigation--open');
+});
